285d0d7cf645b25e72b9aa8b4d2033e67a609ae8
Thomas David Fischer <tdf.tomfischer@gmail.com>
2015-12-17 11:59:43 -0600

    Completed chapter 2.

---
+++ chapter02/hello_world.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    puts("Hello, world!");
+    // puts("something different"); /* Bonus Marks 1 */
+    return 0;
+}
+
+/* 
+   Bonus Marks 2
+   Question: What happens when no main function is given?
+   Answer: Failed compilation.
+*/
+
+/* Bonus Marks 3 & 4 are outside the scope of this program. */
